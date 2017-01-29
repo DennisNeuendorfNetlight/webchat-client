035049cc6a260d4863bcdc3d22a0c22a924b857c
Dennis Neuendorf <dene@netlight.com>
2017-01-29 21:57:38 +0100

    fix reconnect update

--- src/app/reducer/contacts-reducer.ts
+++ src/app/reducer/contacts-reducer.ts
@@ -1,24 +1,27 @@
 import { ActionReducer, Action } from '@ngrx/store';
-import { Message, Contact } from '../models';
+import { Contact } from '../models';
 
-export const ADD_CONTACT = 'ADD_CONTACT';
+export const ADD_CONTACTS = 'ADD_CONTACTS';
 
-export class AddContactAction implements Action {
-    type:string = ADD_CONTACT;
-    constructor(public payload: Message){}
+export class AddContactsAction implements Action {
+    type:string = ADD_CONTACTS;
+    constructor(public payload: Contact[]){}
 }
 
 export const contactsReducer: ActionReducer<Contact[]> = (state=[], {type,payload}) => {
     switch (type) {
-        case ADD_CONTACT:{
-            if(!state.find((contact) => contact.sessionId === payload.sessionId)){  
+        case ADD_CONTACTS:{
+            const findContact = (contact) => contact.sessionId === payload.sessionId;
+            return payload.map(newcontact => state.find(findContact)||newcontact);
+            /*
+            if(!state.find(){  
                 if(state.find((contact) => contact.username === payload.username)){
                     return state.map((contact) => contact.username === payload.username ? Object.assign({}, contact, payload) : contact);
                 }
                 return [...state,payload];
                 //.sort((a:Contact,b:Contact) => a.username<b.username?-1:a.username>b.username?1:0);
             }
-            return state;
+            return state;*/
         }
     }
     return state;
--- src/app/socket/socket-io.service.ts
+++ src/app/socket/socket-io.service.ts
@@ -1,47 +1,48 @@
 import { Injectable } from "@angular/core";
 import * as socketIo from 'socket.io-client';
 import { Store } from '@ngrx/store';
-import { AddContactAction } from '../reducer/contacts-reducer';
+import { AddContactsAction } from '../reducer/contacts-reducer';
 import { ReceiveMessageAction } from '../effects/message-effects';
 import { Contact } from '../models';
 
 @Injectable()
-export class SocketIOService{
+export class SocketIOService {
 
-    private socket: any;
+	private socket: any;
 	private sessionId: string;
 	private username: string;
 
 	constructor(private appStateStore: Store<any>) {
-        this.socket = socketIo.connect('http://localhost:4000');
-        this.socket.on('session', (data) => {
+		this.socket = socketIo.connect('http://localhost:4000');
+		this.socket.on('session', (data) => {
 			this.sessionId = data.sessionId;
-			if (this.username && this.sessionId){
+			if (this.username && this.sessionId) {
 				this.registerClient();
 			}
-		 });
-		 this.socket.on('clients', (clients) => {
-			if(clients && clients.length > 0)
-				clients.filter((client) => client.username != this.username)
-					.every((client) => this.appStateStore.dispatch(new AddContactAction(client)));
-		 });
-		 this.socket.on('chat', (message) => {
+		});
+		this.socket.on('clients', (clients) => {
+			console.log('clients',clients);
+			if (clients && clients.length > 0){
+				this.appStateStore.dispatch(new AddContactsAction(clients.filter((client) => client.username != this.username)));
+			}
+		});
+		this.socket.on('chat', (message) => {
 			this.appStateStore.dispatch(new ReceiveMessageAction(message))
-		 });
-    }
+		});		
+	}
 
-	setUsername(username){
+	setUsername(username) {
 		this.username = username;
-		if (this.username && this.sessionId){
+		if (this.username && this.sessionId) {
 			this.registerClient();
 		}
 	}
 
-	registerClient(){
-		this.socket.emit('register',{username: this.username, sessionId: this.sessionId});
+	registerClient() {
+		this.socket.emit('register', { username: this.username, sessionId: this.sessionId });
 	}
 
-	send(channel:string, message:any){
-		this.socket.emit(channel,message);
+	send(channel: string, message: any) {
+		this.socket.emit(channel, message);
 	}
 }
