18c9060c66c077a2b11d0642e2c37e4b4f3a0e3d
Dennis Neuendorf <dene@netlight.com>
2017-02-13 21:49:28 +0100

    rewrote worker with functions

--- src/rsa-worker.js
+++ src/rsa-worker.js
@@ -1,30 +1,44 @@
 importScripts('cryptico.min.js');
-self.addEventListener('message', function(e){
-    let result;
+
+self.generateRSAKey = (payload) => {
+    const time = new Date();
+    self.privateKey = cryptico.generateRSAKey(payload, 1024);
+    result = {
+        publicKey: cryptico.publicKeyString(privateKey)
+    }; 
+    const aftertime = new Date();
+    console.log('duration generateKeys:', aftertime-time);
+    return result;
+}
+
+self.encrypt = (payload) => {
+    const time = new Date();
+    let encresult = cryptico.encrypt(payload.message.message, payload.publicKey);
+    result = Object.assign({}, payload.message, { message: encresult.cipher});
+    const aftertime = new Date();
+    console.log('duration encrypt:', aftertime-time);
+    return result;
+}
+
+self.decrypt = (payload) => {
+    const time = new Date();
+    let decresult = cryptico.decrypt(payload.message.message, self.privateKey);
+    result = Object.assign({}, payload.message, { message: decresult.plaintext});
+    const aftertime = new Date();
+    console.log('duration decrypt:', aftertime-time);
+    return result;
+}
+
+
+self.onmessage = (e) => {
     let type = e.data.type;
     let payload = e.data.payload;
-    if(type == 'generateKeys' && payload ){
-        const time = new Date();
-        self.privateKey = cryptico.generateRSAKey(payload, 1024);
-        result = {
-            publicKey: cryptico.publicKeyString(privateKey)
-        }; 
-        const aftertime = new Date();
-        console.log('duration generateKeys:', aftertime-time);
+    switch(type){
+        case 'generateKeys':  
+            return self.postMessage(self.generateRSAKey(payload));
+        case 'encrypt':  
+            return self.postMessage(self.encrypt(payload));
+        case 'decrypt':  
+            return self.postMessage(self.decrypt(payload));
     }
-    if(type == 'encrypt' && payload ){
-        const time = new Date();
-        let encresult = cryptico.encrypt(payload.message.message, payload.publicKey);
-        result = Object.assign({}, payload.message, { message: encresult.cipher});
-        const aftertime = new Date();
-        console.log('duration encrypt:', aftertime-time);
-    }
-    if(type == 'decrypt' && payload ){
-        const time = new Date();
-        let decresult = cryptico.decrypt(payload.message.message, self.privateKey);
-        result = Object.assign({}, payload.message, { message: decresult.plaintext});
-        const aftertime = new Date();
-        console.log('duration decrypt:', aftertime-time);
-    }
-    self.postMessage(result);
-});
+};
