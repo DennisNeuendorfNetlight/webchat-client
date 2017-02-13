e8700d8552ea552503bcaf710cd89b89bb5b0963
Dennis Neuendorf <dene@netlight.com>
2017-02-13 21:49:40 +0100

    renamed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    title = "Horsy's - WonderChat";
+    title = "Horsy's - CryptoChat";
 }
